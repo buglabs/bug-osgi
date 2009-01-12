485d022b8738693a0922555aa61fbc4de7e65795
kgilmer <kgilmer@35ad6e01-222d-0410-b96a-b41bba574936>
2009-01-12 18:33:04 +0000

    com.buglabs.bug.module.*: migrated from gplv2 to bsd license and code cleanup.
    git-svn-id: svn://bugcamp.net/bug/trunk/com.buglabs.bug.module.lcd@7919 35ad6e01-222d-0410-b96a-b41bba574936

--- com/buglabs/bug/module/lcd/pub/LCDModlet.java
+++ com/buglabs/bug/module/lcd/pub/LCDModlet.java
@@ -1,3 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2009 Bug Labs, Inc.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *    - Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *    - Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *    - Neither the name of Bug Labs, Inc. nor the names of its contributors may be
+ *      used to endorse or promote products derived from this software without
+ *      specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
 package com.buglabs.bug.module.lcd.pub;
 
 import java.awt.Frame;
@@ -22,7 +49,7 @@ import com.buglabs.bug.menu.pub.StatusBarUtils;
 import com.buglabs.bug.module.lcd.Activator;
 import com.buglabs.bug.module.lcd.accelerometer.LCDAccelerometerInputStreamProvider;
 import com.buglabs.bug.module.lcd.accelerometer.LCDAccelerometerSampleProvider;
-import com.buglabs.bug.module.motion.pub.AccelerationWS;
+import com.buglabs.bug.module.motion.AccelerationWS;
 import com.buglabs.bug.module.pub.IModlet;
 import com.buglabs.module.IModuleControl;
 import com.buglabs.module.IModuleLEDController;
@@ -71,7 +98,7 @@ public class LCDModlet implements IModlet, ILCDModuleControl, IModuleControl, IM
 			{ false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false },
 			{ false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false },
 			{ false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false } };
-	
+
 	private LCDAccelerometerInputStreamProvider lcd_acc_is_prov;
 	private ServiceRegistration accIsProvRef;
 	private ServiceRegistration accRawFeedProvRef;
@@ -221,7 +248,7 @@ public class LCDModlet implements IModlet, ILCDModuleControl, IModuleControl, IM
 	public int enable() throws IOException {
 		return Activator.getInstance().getLCDControl().ioctl_BMI_LCD_CLRRST(slotId);
 	}
-	
+
 	public int setBlackLight(int val) throws IOException {
 		return setBackLight(val);
 	}
