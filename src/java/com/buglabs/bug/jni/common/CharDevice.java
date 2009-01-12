075037010d9ea8a105c6064c004d25703fe6a2e2
kgilmer <kgilmer@35ad6e01-222d-0410-b96a-b41bba574936>
2009-01-12 18:32:15 +0000

    com.buglabs.bug.jni.*: migrated from gplv2 to bsd license and code cleanup.
    git-svn-id: svn://bugcamp.net/bug/trunk/com.buglabs.bug.jni.common@7918 35ad6e01-222d-0410-b96a-b41bba574936

--- src/java/com/buglabs/bug/jni/common/CharDevice.java
+++ src/java/com/buglabs/bug/jni/common/CharDevice.java
@@ -1,3 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2009 Bug Labs, Inc.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *    - Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *    - Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *    - Neither the name of Bug Labs, Inc. nor the names of its contributors may be
+ *      used to endorse or promote products derived from this software without
+ *      specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
 package com.buglabs.bug.jni.common;
 
 public class CharDevice {
@@ -13,23 +40,27 @@ public class CharDevice {
 	}
 
 	public native int open(String file, int mode);
+
 	public native String getline();
+
 	public native int read();
 
 	public int read(byte[] b) {
 		return readBytes(b);
 	}
-	
+
 	/**
-	 * 	There's a bug somewhere (gcc, libc, dlopen, or jvm) where 
-	 *	overloaded functions don't map well through jni.
-	 *	Therefore, the function overloading is at the java layer
-	 *	and renamed the native method to readBytes
+	 * There's a bug somewhere (gcc, libc, dlopen, or jvm) where overloaded
+	 * functions don't map well through jni. Therefore, the function overloading
+	 * is at the java layer and renamed the native method to readBytes
 	 */
 	private native int readBytes(byte[] b);
 
 	public native long lseek(long offset, int whence);
+
 	public native long write(byte[] buf, long count);
+
 	public native int ioctl(int request);
+
 	public native int close();
 }
