4a0c50263d2150c38e8ec53ea4ea8e98369c9a49
kgilmer <kgilmer@35ad6e01-222d-0410-b96a-b41bba574936>
2009-01-12 18:32:15 +0000

    com.buglabs.bug.jni.*: migrated from gplv2 to bsd license and code cleanup.
    git-svn-id: svn://bugcamp.net/bug/trunk/com.buglabs.bug.jni.gps@7918 35ad6e01-222d-0410-b96a-b41bba574936

--- src/java/com/buglabs/bug/jni/gps/GPS.java
+++ src/java/com/buglabs/bug/jni/gps/GPS.java
@@ -1,55 +1,65 @@
-/* Copyright (c) 2007, 2008 Bug Labs, Inc.
+/*******************************************************************************
+ * Copyright (c) 2008, 2009 Bug Labs, Inc.
  * All rights reserved.
- *   
- * This program is free software; you can redistribute it and/or  
- * modify it under the terms of the GNU General Public License version  
- * 2 only, as published by the Free Software Foundation.   
- *   
- * This program is distributed in the hope that it will be useful, but  
- * WITHOUT ANY WARRANTY; without even the implied warranty of  
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU  
- * General Public License version 2 for more details (a copy is  
- * included at http://www.gnu.org/licenses/old-licenses/gpl-2.0.html).   
- *   
- * You should have received a copy of the GNU General Public License  
- * version 2 along with this work; if not, write to the Free Software  
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  
- * 02110-1301 USA   
- *
- */
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *    - Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *    - Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *    - Neither the name of Bug Labs, Inc. nor the names of its contributors may be
+ *      used to endorse or promote products derived from this software without
+ *      specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
 package com.buglabs.bug.jni.gps;
 
 import com.buglabs.bug.jni.common.CharDevice;
 
 public class GPS extends CharDevice {
-	
-	//Control LEDs
+
+	// Control LEDs
 	public static final int BMI_GPS_RLEDOFF = 0x1;
 	public static final int BMI_GPS_RLEDON = 0x2;
 	public static final int BMI_GPS_GLEDOFF = 0x3;
 	public static final int BMI_GPS_GLEDON = 0x4;
-	
-	//Don't care about these
+
+	// Don't care about these
 	public static final int BMI_GPS_SETBOOT = 0x5;
 	public static final int BMI_GPS_CLRBOOT = 0x6;
 	public static final int BMI_GPS_SETWAKE = 0x7;
 	public static final int BMI_GPS_CLRWAKE = 0x8;
 	public static final int BMI_GPS_SETRST = 0xA;
 	public static final int BMI_GPS_CLRRST = 0xB;
-	
-	//Passive is the internal antenna
-	//Active is the external antenna
-	//Default is passive.
+
+	// Passive is the internal antenna
+	// Active is the external antenna
+	// Default is passive.
 	public static final int BMI_GPS_ACTIVE_ANT = 0xC;
-	public static final int BMI_GPS_PASSIVE_ANT = 0xD;	
-	
+	public static final int BMI_GPS_PASSIVE_ANT = 0xD;
+
 	/**
 	 * Initializes the serial port the GPS device is on.
-	 * @param fd the file descriptor of the GPS device
+	 * 
+	 * @param fd
+	 *            the file descriptor of the GPS device
 	 * @return
 	 */
 	public native int init();
-	
+
 	static {
 		System.loadLibrary("GPS");
 	}
