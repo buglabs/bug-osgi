9535472af742325a09311eed6eab8aaacfdb03ee
kgilmer <kgilmer@35ad6e01-222d-0410-b96a-b41bba574936>
2009-01-12 18:33:04 +0000

    com.buglabs.bug.module.*: migrated from gplv2 to bsd license and code cleanup.
    git-svn-id: svn://bugcamp.net/bug/trunk/com.buglabs.bug.module.motion@7919 35ad6e01-222d-0410-b96a-b41bba574936

--- com/buglabs/bug/module/motion/MotionSubject.java
+++ com/buglabs/bug/module/motion/MotionSubject.java
@@ -1,3 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2009 Bug Labs, Inc.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *    - Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *    - Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *    - Neither the name of Bug Labs, Inc. nor the names of its contributors may be
+ *      used to endorse or promote products derived from this software without
+ *      specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
 package com.buglabs.bug.module.motion;
 
 import java.io.IOException;
@@ -23,21 +50,18 @@ public class MotionSubject extends Thread implements IMotionSubject {
 		byte read = 0;
 
 		try {
-			while(!isInterrupted() && (read = (byte) motionIs.read()) != -1) {
-				byte expected = Motion.BMI_MOTION_DETECT_ENABLED | 
-								Motion.BMI_MOTION_DETECT_DELTA | 
-								Motion.BMI_MOTION_DETECT_LATCHED_STATUS | 
-								Motion.BMI_MOTION_DETECT_STATUS;
-				
-				if( read == expected) {
+			while (!isInterrupted() && (read = (byte) motionIs.read()) != -1) {
+				byte expected = Motion.BMI_MOTION_DETECT_ENABLED | Motion.BMI_MOTION_DETECT_DELTA | Motion.BMI_MOTION_DETECT_LATCHED_STATUS | Motion.BMI_MOTION_DETECT_STATUS;
+
+				if (read == expected) {
 					notifyObservers();
 				}
 			}
 		} catch (IOException e) {
 			// TODO Log this
-			//e.printStackTrace();
+			// e.printStackTrace();
 		} finally {
-			if(motionIs != null) {
+			if (motionIs != null) {
 				try {
 					motionIs.close();
 				} catch (IOException e) {
@@ -45,13 +69,13 @@ public class MotionSubject extends Thread implements IMotionSubject {
 					e.printStackTrace();
 				}
 			}
-		} 
+		}
 	}
 
 	public void notifyObservers() {
-		synchronized(observers) {
+		synchronized (observers) {
 			Iterator iter = observers.iterator();
-			while(iter.hasNext()) {
+			while (iter.hasNext()) {
 				IMotionObserver obs = (IMotionObserver) iter.next();
 				obs.motionDetected();
 			}
@@ -59,13 +83,13 @@ public class MotionSubject extends Thread implements IMotionSubject {
 	}
 
 	public void register(IMotionObserver obs) {
-		synchronized(observers) {
+		synchronized (observers) {
 			observers.add(obs);
 		}
 	}
 
 	public void unregister(IMotionObserver obs) {
-		synchronized(observers) {
+		synchronized (observers) {
 			observers.remove(obs);
 		}
 	}
