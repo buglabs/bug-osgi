5149378dd628b5e5666803c746e7a2bd8007adec
kgilmer <kgilmer@35ad6e01-222d-0410-b96a-b41bba574936>
2010-09-01 14:39:12 +0000

    com.buglabs.base: comments, code cleanup to LED API.
    git-svn-id: svn://bugcamp.net/bug/trunk/com.buglabs.bug.base.molly@11964 35ad6e01-222d-0410-b96a-b41bba574936

--- com/buglabs/bug/sysfs/LEDDevice.java
+++ com/buglabs/bug/sysfs/LEDDevice.java
@@ -3,33 +3,59 @@ package com.buglabs.bug.sysfs;
 import java.io.File;
 import java.io.IOException;
 
+import com.buglabs.bug.sysfs.SysfsNode;
+
 /**
- * A class to control a single-color LED.
+ * A class to control a single-color or multi-color LED.  
  * 
  * @author kgilmer
  * 
  */
 public class LEDDevice extends SysfsNode {
 
+	/**
+	 * LED is single color.
+	 */
 	public static final int TYPE_MONO_COLOR = 1;
+	/**
+	 * LED is tri color
+	 */
 	public static final int TYPE_TRI_COLOR = 2;
 
+	/**
+	 * Constant for single color LED
+	 */
 	public static final int COLOR_MONO = 0;
+	
+	/**
+	 * Color constants for tri color LEDs.
+	 */
 	public static final int COLOR_RED = 0;
 	public static final int COLOR_GREEN = 1;
 	public static final int COLOR_BLUE = 2;
 
+	/**
+	 * Used in the construction of file paths for LED sysfs API in Linux .
+	 */
 	private static final String FILE_BRIGHTNESS = "brightness";
 	private static final String FILE_TRIGGER = "trigger";
 	private static final String FILE_MAX_BRIGHTNESS = "max_brightness";
 	private static final String PLATFORM_NAME = "omap3bug";
 
-	/*
-	 * omap3bug:blue:battery omap3bug:green:battery omap3bug:blue:power
-	 * omap3bug:blue:bt omap3bug:red:wlan omap3bug:blue:wlan omap3bug:green:wlan
+	/**
+	 * Name of LED in sysfs
 	 */
 	private final String name;
+	
+	/**
+	 * Number of colors LED supports
+	 */
 	private final int type;
+	
+	
+	/**
+	 * Arrays to access files to control and read LED state.
+	 */
 	private final File[] ledBase;
 	private final File[] ledBrightness;
 	private final File[] ledMaxBrightness;
@@ -115,18 +141,39 @@ public class LEDDevice extends SysfsNode {
 		return PLATFORM_NAME + ":" + color + ":" + n;
 	}
 
+	/**
+	 * Set the brightness for the specified LED.
+	 * @param color
+	 * @param brightness
+	 * @throws IOException
+	 */
 	public void setBrightness(int color, int brightness) throws IOException {
 		println(ledBrightness[color], "" + brightness);
 	}
 
+	/**
+	 * @param color
+	 * @return the current brightness of the specified LED.
+	 */
 	public int getBrightness(int color) {
 		return Integer.parseInt(getFirstLineofFile(ledBrightness[color]));
 	}
 
+	/**
+	 * Set a trigger on a LED.
+	 * @param color
+	 * @param trigger
+	 * @throws IOException
+	 */
 	public void setTrigger(int color, String trigger) throws IOException {
 		println(ledTrigger[color], trigger);
 	}
 
+	/**
+	 * @param color
+	 * @return The LED trigger that is currently set.  "none" means no trigger is active.
+	 * @throws IOException
+	 */
 	public String getLEDTrigger(int color) throws IOException {
 		String line = getFirstLineofFile(ledTrigger[color]);
 
@@ -136,6 +183,12 @@ public class LEDDevice extends SysfsNode {
 		return elems[0];
 	}
 
+	/**
+	 * Get the Trigger types this LED supports.  These trigger types can be set using setLEDTrigger.
+	 * @param color
+	 * @return
+	 * @throws IOException
+	 */
 	public String[] getLEDTriggers(int color) throws IOException {
 		String line = getFirstLineofFile(ledTrigger[color]);
 
@@ -143,12 +196,19 @@ public class LEDDevice extends SysfsNode {
 
 		for (int i = 0; i < elems.length; ++i) {
 			if (elems[i].startsWith("[")) {
-				elems[i] = elems[i].substring(1, elems[i].length() - 2);
+				elems[i] = elems[i].substring(1, elems[i].length() - 1);
 				// Only one trigger is set, so we only have to strip this once.
 				break;
 			}
 		}
 
 		return elems;
+	}
+	
+	/**
+	 * @return The number of colors this LED supports.  Zero based.
+	 */
+	public int getColorCount() {
+		return ledBase.length;
 	}
 }
