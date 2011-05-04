fd68e7db6079a16f629d56cafe6d032dd54ee580
Ken Gilmer <kgilmer@gmail.com>
2011-05-04 12:17:05 +0900

    com.buglabs.util.shell: extracting non hw related functionality from com.buglabs.bug.base into isolated bundles.

---
+++ com.buglabs.util.shell/src/com/buglabs/util/shell/Activator.java
@@ -0,0 +1,37 @@
+package com.buglabs.util.shell;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+import com.buglabs.util.shell.pub.IShellService;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	private ServiceRegistration sr;
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		sr = context.registerService(IShellService.class.getName(), new ShellService(), null);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		sr.unregister();
+		Activator.context = null;
+	}
+
+}
