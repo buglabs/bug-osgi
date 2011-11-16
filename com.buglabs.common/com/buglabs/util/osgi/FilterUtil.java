buglabs/bug-osgi

commit 9706e7541ab4dd2422518b0d98693d19c414b10a

buglabs-osgi: remove redundant Arrays.asList().

com.buglabs.common/com/buglabs/util/osgi/FilterUtil.java
@@ -64,7 +64,7 @@ public final class FilterUtil {
 	 * @return A LDAP filter string that can be used to construct a Filter.  If null passed in, null is returned to caller.
 	 */
 	public static String generateServiceFilter(String ... services) {
-		return generateServiceFilter(Arrays.asList(services));
+		return generateServiceFilter(services);
 	}
 	
 	/**
@@ -98,9 +98,7 @@ public final class FilterUtil {
 	 * @throws InvalidSyntaxException thrown if a syntax error is found in the generated Filter.
 	 */
 	public static Filter generateServiceFilter(BundleContext context, String[] services) throws InvalidSyntaxException {
-		String filterString = generateServiceFilter(Arrays.asList(services));
-
-		return context.createFilter(filterString);
+		return context.createFilter(generateServiceFilter(services));
 	}
 
 	/**

com.buglabs.common/com/buglabs/util/osgi/ServiceTrackerUtil.java
@@ -159,7 +159,7 @@ public class ServiceTrackerUtil implements ServiceTrackerCustomizer {
 		Dictionary<String, Object> dict = new Hashtable<String, Object>();
 		
 		if (arg0.getPropertyKeys() != null) {
-			for (String key : Arrays.asList(arg0.getPropertyKeys())) {
+			for (String key : arg0.getPropertyKeys()) {
 				dict.put(key, arg0.getProperty(key));
 			}
 		}

com.buglabs.osgi.sewing/com/buglabs/osgi/sewing/pub/SewingController.java
@@ -187,7 +187,7 @@ public abstract class SewingController {
 	 * @return true if all keys are present, false otherwise
 	 */
 	protected final String getMissingParameter(RequestParameters params, String ... requiredKeys) {
-		for (String key: Arrays.asList(requiredKeys))
+		for (String key: requiredKeys)
 			if (params.get(key) == null)
 				return key;
 		

com.buglabs.osgi.tester/com/buglabs/osgi/tester/BundleTestRunnerThread.java
@@ -55,7 +55,7 @@ public class BundleTestRunnerThread extends Thread {
 
 			if (srefs != null && srefs.length > 0) {				
 
-				for (ServiceReference sr : Arrays.asList(srefs)) {
+				for (ServiceReference sr : srefs) {
 					TestSuite ts = (TestSuite) context.getService(sr);
 
 					if (ts != null)
@@ -80,7 +80,7 @@ public class BundleTestRunnerThread extends Thread {
 		}
 		
 		//Shutdown all the bundles
-		for (Bundle bundle : Arrays.asList(context.getBundles())) {
+		for (Bundle bundle : context.getBundles()) {
 			if (bundle.getBundleId() != 0) {
 				try {
 					bundle.stop();

commit 4175c64ec814f1504e50d1dea7aa5b3eb3447d30

bug-osgi: fix infinite recursion due to incorrect method overloading introduced in 9706e7541ab4dd2422518b0d98693d19c414b10a.  See https://github.com/buglabs/bug-osgi/commit/9706e7541ab4dd2422518b0d98693d19c414b10a#commitcomment-722303

com.buglabs.common/com/buglabs/util/osgi/FilterUtil.java
@@ -64,7 +64,7 @@ public final class FilterUtil {
 	 * @return A LDAP filter string that can be used to construct a Filter.  If null passed in, null is returned to caller.
 	 */
 	public static String generateServiceFilter(String ... services) {
-		return generateServiceFilter(services);
+		return generateServiceFilter(Arrays.asList(services));
 	}
 	
 	/**
@@ -98,7 +98,9 @@ public final class FilterUtil {
 	 * @throws InvalidSyntaxException thrown if a syntax error is found in the generated Filter.
 	 */
 	public static Filter generateServiceFilter(BundleContext context, String[] services) throws InvalidSyntaxException {
-		return context.createFilter(generateServiceFilter(services));
+		 String filterString = generateServiceFilter(Arrays.asList(services));
+		 
+		 return context.createFilter(filterString);
 	}
 
 	/**
@@ -116,8 +118,8 @@ public final class FilterUtil {
 	 */
 	public static String generateServiceFilter(SortedMap<String, Map<String, String>> servicesMap) {
 		if (servicesMap.size() == 1) {
-			return "(&(" + Constants.OBJECTCLASS + "=" + ((String) servicesMap.firstKey()) + ")"
-					+ generatePropertiesFilter(new TreeMap((Map) servicesMap.get(servicesMap.firstKey()))) + ")";
+			return "(&(" + Constants.OBJECTCLASS + "=" + servicesMap.firstKey() + ")"
+					+ generatePropertiesFilter(new TreeMap(servicesMap.get(servicesMap.firstKey()))) + ")";
 		} else if (servicesMap.size() > 1) {
 			return "(|" + generateServiceFilter(servicesMap.subMap(servicesMap.firstKey(), servicesMap.firstKey() + "\0"))
 					+ generateServiceFilter(servicesMap.tailMap(servicesMap.firstKey() + "\0")) + ")";